df4f93f433917f685d41eeb588ff1141f2cff769
Viner Gataullin <94351574+imyapolza@users.noreply.github.com>
2023-02-12 22:59:57 +0500

    cors error solved

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,24 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const config = await app.get(ConfigService);
-  const port = config.get<number>('API_PORT');
-
-  console.log('port', port);
+  const port = config.get<number>("API_PORT");
 
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
-  };
-
-  app.enableCors(options);
+  app.enableCors({
+    origin: config.get<string>("API_FRONTEND_URL"),
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true
+  });
 
   await app.listen(port || 8000, () => {
     console.log(`App started on port: ${port || 8000}`);
